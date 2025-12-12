922a2666c436e081e9bfbc450b4bc9a09e3d3151
Marvelbad <badra.aqua@gmail.com>
2025-12-12 13:59:12 +0300

    in progress

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -4,7 +4,10 @@ import com.example.demo.repository.User;
 import com.example.demo.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -17,12 +20,15 @@ public class UserService {
     }
 
     public List<User> findAll() {
-        userRepository.findByEmail(user.getEmail());
         return userRepository.findAll();
     }
 
     public User create(User user) {
-
+        Optional<User> optionalUser = userRepository.findByEmail(user.getEmail());
+        if (optionalUser.isPresent()) {
+            throw new IllegalStateException("User with such email already exists");
+        }
+        user.setAge(Period.between(user.getBirth(), LocalDate.now()).getYears());
         return userRepository.save(user);
     }
 }
