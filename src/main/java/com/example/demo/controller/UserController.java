344d06d8d44b17ab70b9ea5d000afa127901450d
Marvelbad <badra.aqua@gmail.com>
2025-12-12 20:28:53 +0300

    in progress

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,28 +1,47 @@
 package com.example.demo.controller;
 
 import com.example.demo.repository.User;
+import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
+
 @RestController
-@RequestMapping(path="api/users")
+@RequestMapping(path = "api/users")
 public class UserController {
 
     private final UserService userService;
+    private final UserRepository userRepository;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, UserRepository userRepository) {
         this.userService = userService;
+        this.userRepository = userRepository;
     }
 
     @GetMapping
     public List<User> findAll() {
-       return userService.findAll();
+        return userService.findAll();
     }
 
     @PostMapping
     public User create(@RequestBody User user) {
         return userService.create(user);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void delete(@PathVariable Long id) {
+        userService.delete(id);
+    }
+
+    @PutMapping(path = "{id}")
+    public void update(
+            @PathVariable Long id,
+            @RequestParam(required = false) String email,
+            @RequestParam(required = false) String name) {
+
+        userService.update(id, email, name );
     }
 }
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -2,12 +2,20 @@ package com.example.demo.service;
 
 import com.example.demo.repository.User;
 import com.example.demo.repository.UserRepository;
+import jakarta.persistence.metamodel.SingularAttribute;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.domain.AbstractPersistable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.List;
 import java.util.Optional;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @Service
 public class UserService {
@@ -30,5 +38,36 @@ public class UserService {
         }
         user.setAge(Period.between(user.getBirth(), LocalDate.now()).getYears());
         return userRepository.save(user);
+    }
+
+    public void delete(Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isEmpty()) {
+            throw new IllegalStateException("User with id " + id + " is not exists");
+        }
+        userRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void update(Long id, String email, String name) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isEmpty()) {
+            throw new IllegalStateException("User with id " + id + " is not exists");
+        }
+        User user = optionalUser.get();
+
+        if (email != null && !email.equals(user.getEmail())) {
+            Optional<User> foundByEmail = userRepository.findByEmail(email);
+            if (foundByEmail.isPresent()) {
+                throw new IllegalStateException("User with such email already exists");
+            }
+            user.setEmail(email);
+        }
+
+        if (name != null && !name.equals(user.getEmail())) {
+            user.setName(name);
+        }
+
+        userRepository.save(user);
     }
 }
